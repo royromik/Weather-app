ba3d218f4d57816a9152af11c5ab635e19177595
royromik <royromik@gmail.com>
2020-12-23 11:45:25 +0530

    geocoding api added

--- app.js
+++ app.js
@@ -9,5 +9,26 @@ const url = `https://api.openweathermap.org/data/2.5/onecall?lat=22.468766&lon=8
 
 request({url: url, json: true}, (error, response) => {
     const data = response.body;
+    if(error){
+        console.log("unable to connect to the geocoing services api")
+    } else if(data.message || data.error){
+        console.log("Unable to find the location")
+    } else{
     console.log(`it is currently ${data.current.temp} degrees out. There is ${data.current.humidity}% humidity.`);
+    }
+})
+
+const geocode_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Kolkata.json?access_token=pk.eyJ1Ijoicm95cm9taWsiLCJhIjoiY2tqMHozd2gzMGQ3NTJ6cXRlNHJ6Nm5jdyJ9.QUf8guX-Y97YroHCVUsOFQ&limit=1'
+
+request({ url: geocode_url, json: true }, (error, response) => {
+    const data = response.body;
+    if(error){
+        console.log("unable to connect to the geocoing services api")
+    } else if(data.message || data.error || data.features.length == 0){
+        console.log("Unable to find the location")
+    } else{
+        const latitude = data.features[0].center[1];
+        const longitude = data.features[0].center[0]
+        console.log(latitude,longitude);
+    }
 })
